7c1791f0ef2c33df205b03e893b5c51e313c2470
damien <damiengabrielle@gmail.com>
2023-08-10 01:21:41 +0200

    fix player position

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -13,8 +13,7 @@ const SPEED : float = 20.0
 const ROTATION_SPEED : float = 3.0
 const MOVEMENT_SPEED : float = 5.0
 const RESET_CLOTH_POSITION : float = 30.0
-const GRID_SIZE : int = 2
-const GRID_WIDTH : int = GRID_SIZE * 2 + 1
+const GRID_WIDTH : int = 5
 const LOWEST_LEVEL : int = 10
 
 var player_pos : Vector2i = Vector2(0, 1)
@@ -31,10 +30,14 @@ func _ready() -> void:
 	reset_level()
 
 func reset_level() -> void:
-	var x : float = GameData.cell_size * (GRID_SIZE + .5)
-	$building/wall1.position.x = -x - .5
-	$building/wall2.position.x = x + .5
-	camera_3d.position.y = (x + .5) / sin(PI / 2.0)
+	var x : float = GameData.cell_size * GRID_WIDTH
+	$building/wall1.position.z = -GameData.cell_size / 2.0
+	$building/wall2.position.z = -GameData.cell_size / 2.0 + x
+	var id = random_cell_id()
+	player_pos = cell_id_to_vector2(id)
+	player.position = cell_position(player_pos)
+	camera_3d.position = player.position
+	camera_3d.position.y = (x + .5) / (2 * sin(PI / 2.0))
 	cube = {}
 	for i in range(LOWEST_LEVEL):
 		cube[i] = []
@@ -53,21 +56,22 @@ func _input(event: InputEvent) -> void:
 func move_level_up() -> void:
 	var material : Material = $building/wall1.mesh.material
 	var tween : Tween = create_tween()
-
 	for block in blocks.get_children():
 		if block is BlockItem:
 			block.tick(level)
 
-	cube[level - 1] = []
-	for i in range(randi() % 3):
+	cube[level] = []
+	for i in range(randi() % 3 + 1):
 		var b = BLOCK.instantiate()
 		blocks.add_child(b)
 		b.set_color(level)
-		var p = random_cell_position(-LOWEST_LEVEL * GameData.cell_size)
-		b.position = p
-		cube[level - 1].push_back(p)
+		var p2 = cell_id_to_vector2(random_cell_id())
+		var p3 = cell_position(p2)
+		p3.y = -(LOWEST_LEVEL - 1) * GameData.cell_size
+		b.position = p3
+		cube[level].push_back(p2)
 
-	tween.tween_property(material, "uv1_offset:y", -level * 0.25 - .1, GameData.TICK_COOLDOWN)
+	tween.tween_property(material, "uv1_offset:y", -level * 0.125 - .1, GameData.TICK_COOLDOWN)
 
 func move_player(x: int, y: int) -> void:
 	if not can_move:
@@ -76,12 +80,14 @@ func move_player(x: int, y: int) -> void:
 	do_move(x, y)
 
 func zone_clamp(vector: Vector2i) -> Vector2i:
-	return vector.clamp(Vector2i.ONE * -GRID_SIZE, Vector2i.ONE * GRID_SIZE)
+	return vector.clamp(Vector2i.ZERO, Vector2i(GRID_WIDTH - 1, GRID_WIDTH - 1))
 
 func new_position_allowed(pos: Vector2i) -> bool:
 	var key : int = level - LOWEST_LEVEL + 1
 	if cube.has(key):
-		print(cube[key])
+		for i in cube[key]:
+			if i == pos:
+				return false
 	return true
 
 func do_move(x: int, y: int) -> void:
@@ -114,7 +120,8 @@ func random_cell_id() -> int:
 	return randi() % (GRID_WIDTH * GRID_WIDTH)
 
 func random_cell_position(height: float) -> Vector3:
-	var l : Array = range(-GRID_SIZE, GRID_SIZE + 1)
+	var l : Array = range(0, GRID_WIDTH)
+	print(l)
 	var p : Vector3 = cell_position(Vector2i(l.pick_random(), l.pick_random()))
 	p.y = height
 	return p
